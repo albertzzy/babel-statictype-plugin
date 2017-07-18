daf571820ead416dbc5fd1b488ff975706a327dc
albertzzy <zhaozi_ye_hi@126.com>
2017-07-18 23:19:50 +0800

    lalal

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-module.exports.exp = 12;
+// module.exports.exp = 12;
+
+
+function printTips(a,b) {
+  tips.forEach((tip, i) => console.log(`Tip ${i}:` + tip));
+}
+
+export default printTips;
+module.exports = printTips;
+
+
 
 /*
 const demoVisitor = {
 
 	Identifier(path){
-		console.log(path.node)
+		// console.log(path.node)
 	},	
 
 	ExportNamedDeclaration(path,state){
 
 		// console.log(path)
+	},
+	FunctionDeclaration(path,state){
+
+		// console.log(path.node.params)
+
+	},
+	ExportDefaultDeclaration(path,state){
+		// console.log(state)
+		// console.log(path.node.declaration.loc.identifierName)
 	}
 
 
@@ -24,8 +43,8 @@ module.exports = function({types:t}){
 		visitor:{
 
 			Program(path,state){
-
-				path.traverse(demoVisitor,state)
+				path.insertBefore(t.expressionStatement(t.stringLiteral('a=1')))
+				// path.traverse(demoVisitor,state)
 
 			}
 
@@ -34,6 +53,5 @@ module.exports = function({types:t}){
 
 	}
 }
-
 
 */
--- post-index.js
+++ post-index.js
@@ -1,1 +1,47 @@
-module.exports.exp = 12;
+// module.exports.exp = 12;
+
+
+function printTips(a, b) {
+	tips.forEach((tip, i) => console.log(`Tip ${i}:` + tip));
+}
+
+export default printTips;
+module.exports = printTips;
+
+/*
+const demoVisitor = {
+
+	Identifier(path){
+		console.log(path.node)
+	},	
+
+	ExportNamedDeclaration(path,state){
+
+		// console.log(path)
+	}
+
+
+
+}
+ 
+
+
+module.exports = function({types:t}){
+	return {
+
+		visitor:{
+
+			Program(path,state){
+
+				path.traverse(demoVisitor,state)
+
+			}
+
+		}
+
+
+	}
+}
+
+
+*/
