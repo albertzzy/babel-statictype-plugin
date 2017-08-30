018caad3b681639c2036b4fe2d06763fba7a7a16
albertzzy <zhaozi_ye_hi@126.com>
2017-08-30 15:04:34 +0800

    revise test

--- test/index.test.js
+++ test/index.test.js
@@ -1,29 +1,54 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const expect = require('expect');
+const path = require('path');
 
 async function testNormalFunc(){
     
-    return  await exec('babel fixtures/normal.js --out-dir fixtures/dist --plugins=babel-statictype-plugin');
+    return  await exec('babel '+path.resolve(__dirname,'scripts/normal.js')+' --out-file '+path.resolve(__dirname,'dist/normal.js'));
 
 }
+
+async function testErrorFunc(){
+
+    return  await exec('babel '+path.resolve(__dirname,'scripts/error.js')+' --out-file '+path.resolve(__dirname,'dist/error.js'));
+}
+
 
 
 describe('compile successfully',function(){
     it('should compile successfully',function(done){
 
         testNormalFunc().then((res)=>{
-        console.log(res);
-
-        done();
+            // console.log(res);
+            expect(res.stdout).toBe('');
+            expect(res.stderr).toBe(''); 
+            done();
 
         }).catch((e) => {
             console.log(e);
         })
 
-        
-
     })
 
 })
 
+
+describe('its a error case',function(){
+    it('should pop compile error',function(done){
+
+        testErrorFunc().then((res)=>{
+
+            done();
+
+        }).catch((e) => {
+            // console.log(e);
+
+            expect(e instanceof Error).toBe(true);
+
+            done();
+        })
+
+    })
+
+})
